282271efbca24131a328446d5f39ce2c2cd2e886
Martin Anton Müller <martin.anton.mueller@oeaw.ac.at>
2023-04-06 11:59:03 +0200

    fix for fetch data, added calendar

--- fetch_data.sh
+++ fetch_data.sh
@@ -4,7 +4,7 @@ rm -rf data
 wget https://github.com/arthur-schnitzler/schnitzler-tage/archive/refs/heads/main.zip
 unzip main
 
-mv ./schnitzler-tage-main/data .
+mv ./schnitzler-tage-main/editions/data .
 rm -rf ./data/xslts
 rm main.zip
 rm -rf ./schnitzler-tage-main
