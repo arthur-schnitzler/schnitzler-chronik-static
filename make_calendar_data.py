0df6c637f442e5994c09c96bd73aa9ff40a27c2e
Martin Anton Müller <martin.anton.mueller@oeaw.ac.at>
2023-04-06 12:00:59 +0200

    calendar fix

--- make_calendar_data.py
+++ make_calendar_data.py
@@ -3,7 +3,7 @@ import os
 import json
 from acdh_tei_pyutils.tei import TeiReader
 from tqdm import tqdm
-files = sorted(glob.glob('./data/editions/*.xml'))
+files = sorted(glob.glob('./data/*.xml'))
 out_file = "./html/js-data/calendarData.js"
 data = []
 for x in tqdm(files, total=len(files)):
@@ -12,13 +12,7 @@ for x in tqdm(files, total=len(files)):
     doc = TeiReader(x)
     item['name'] = doc.any_xpath('//tei:title[@level="a"]/text()')[0]
     try:
-        item['startDate'] = doc.any_xpath('//tei:title[@type="iso-date"]/text()')[0]
-    except:
-        continue
-    try:
-        item['tageszaehler'] = doc.any_xpath('//tei:title[@type="iso-date"]/@n')[0]
-        item['id'] = tail.replace('.xml', '.html')
-        data.append(item)
+        item['startDate'] = doc.any_xpath('//tei:title/@when-iso/text()')[0]
     except:
         continue
 
