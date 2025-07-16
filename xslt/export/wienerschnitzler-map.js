2c577767d8fac4d30a680fb4543320455c8dc35c
martinantonmueller <martin.anton.mueller@oeaw.ac.at>
2025-07-16 22:47:17 +0200

    test

---
+++ xslt/export/wienerschnitzler-map.js
@@ -0,0 +1,37 @@
+            const mapElement = document.getElementById("wienerschnitzler-map");
+            const datum = mapElement.getAttribute("data-datum");
+
+            document.addEventListener("DOMContentLoaded", function () {
+  const mapElement = document.getElementById("wienerschnitzler-map");
+  if (!mapElement) return;
+
+  const datum = mapElement.getAttribute("data-datum");
+  const geojsonUrl = `https://raw.githubusercontent.com/wiener-moderne-verein/wienerschnitzler-data/refs/heads/main/data/editions/geojson/${datum}.geojson`;
+
+  const map = L.map("wienerschnitzler-map").setView([48.2082, 16.3738], 6);
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 18,
+    attribution: "© OpenStreetMap",
+  }).addTo(map);
+
+  fetch(geojsonUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      const layer = L.geoJSON(data, {
+        onEachFeature: function (feature, layer) {
+          if (feature.properties && feature.properties.name) {
+            layer.bindPopup(feature.properties.name);
+          }
+        },
+      }).addTo(map);
+      map.fitBounds(layer.getBounds());
+    })
+    .catch((err) => console.error("Fehler beim Laden der GeoJSON-Datei:", err));
+});
+            
+            mapInitialized = true;
+            } else {
+            setTimeout(() => map.invalidateSize(), 200);
+            }
+            });
+            });
